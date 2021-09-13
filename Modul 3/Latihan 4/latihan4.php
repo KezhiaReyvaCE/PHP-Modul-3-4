c38bf91334a54e088fad145e93a93e6527561773
Kezhiareyva <85090045+KezhiaReyvaCE@users.noreply.github.com>
2021-09-13 18:38:28 +0700

    Update latihan4.php

--- Modul 3/Latihan 4/latihan4.php
+++ Modul 3/Latihan 4/latihan4.php
@@ -1,6 +1,8 @@
 <?php
-    $tahun = date ("Y");
-    $kabisat = ($tahun%4 == 0) ? "KABISAT" : "BUKAN 
-    KABISAT";
-    echo "Tahun <b>$tahun</b> $kabisat";
+    $user="";
+    if (!isset($user)) {
+        echo "Variabel tidak ada/belum terbentuk";
+    } else {
+        echo "Variabel ada";
+    }      
 ?>
